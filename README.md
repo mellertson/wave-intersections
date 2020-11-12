332e20cc0a3bd08c5e27ceb37b0b1d1a4eb0329a
Mike Ellertson <mdellertson@gmail.com>
2020-11-12 00:09:33 -0800

    added a few graphics to the README

---
+++ README.md
@@ -0,0 +1,39 @@
+# Overview
+
+This is a curiosity project, which I threw toghter.  It allows you to visualize 
+how waves from multiple wave generators interfere with each other.  The 
+original idea behind this repo was to see how high frequency waves 
+from Tesla coils might interact with each other. 
+
+# Install
+
+You will need Python 3 installed.  Then run `pip install -r requirements.txt`
+
+# How to use
+
+There are some variables at the top of `main.py`, which you can "twiddle". 
+Then just execute `main.py`.  A separate PNG file will be saved to the 
+project's root directory with a filename like the following.
+
+`waves-#_sides-#_hertz-#.png`
+
+# Example Output
+
+Here are a few of my favorited images, which it generated.  
+
+This one shows a "V" patter at the top and bottom, which look like a pattern
+I saw formed from very small waves while the wave was washing back out into
+the ocean a few weeks ago. Seeing those patterns in the ocean was part of the
+reason I wrote this code.  I was hoping to reproduce those same patters, which 
+I have done in this image.
+![image](https://user-images.githubusercontent.com/1175253/98911171-09439980-2479-11eb-9bfd-80cbddadb4fd.png)
+
+This one just looks cool.  You can change the colors around if it is 
+difficult to look.
+![image](https://user-images.githubusercontent.com/1175253/98911758-fb424880-2479-11eb-9eb8-a26e264be63c.png)
+
+This one has some interesting looking color streaks, which seem to bend
+slightly.
+![image](https://user-images.githubusercontent.com/1175253/98912388-e4e8bc80-247a-11eb-8a09-44d856372559.png)
+
+
--- main.py
+++ main.py
@@ -1,12 +1,27 @@
+#!/usr/bin/env python3
 import matplotlib.pyplot as plt
 import math
 
 
-HERTZ = range(1, 10)
-SIDES = range(3, 4)
+HERTZ = range(5, 21)
+SIDES = range(3, 10)
 CANVAS_SIZE = [-10, 10]
-WAVES = 200
-COLORS = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black',]
+WAVES = 350
+COLORS = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow',]
+ALPHA = 0.75
+
+
+class Colors(object):
+	""" Iterates over matplotlib colors """
+	def __init__(self):
+		self.i = 0
+	def __iter__(self):
+		return self
+	def __next__(self):
+		self.i += 1
+		if self.i >= len(COLORS):
+			self.i = 0
+		return COLORS[self.i]
 
 
 def get_vertices(sides):
@@ -23,8 +38,17 @@ def get_vertices(sides):
 def get_waves(vertex, hertz, color):
 	circles = []
 	delta = 1/hertz
+	circles.append(plt.Circle(vertex, 0.075, color='black', fill=True))
 	for i in range(WAVES):
-		circles.append(plt.Circle(vertex, delta * i, color=color, fill=False))
+		circles.append(
+			plt.Circle(
+				vertex,
+				delta * i,
+				color=color,
+				fill=False,
+				alpha=ALPHA,
+			)
+		)
 	return circles
 
 
@@ -32,8 +56,9 @@ def create_plot(sides, hertz):
 	fig, ax = plt.subplots()
 	ax.add_artist(plt.Circle((0,0), 0.05, color='black', fill=True))
 	vertices = get_vertices(sides)
+	colors = Colors()
 	for vertex in vertices:
-		color = COLORS[vertices.index(vertex)]
+		color = colors.__next__()
 		waves = get_waves(vertex, hertz, color)
 		for wave in waves:
 			ax.add_artist(wave)
@@ -41,10 +66,11 @@ def create_plot(sides, hertz):
 	fig.canvas.draw()
 	plt.ylim(CANVAS_SIZE)
 	plt.xlim(CANVAS_SIZE)
-	fig.savefig(f'waves-{sides}-sides.{hertz}-hertz.png')
+	plt.title(f'{sides} Equidistant Waves at {hertz}Hz')
+	fig.savefig(f'waves-{WAVES}_sides-{sides}_hertz-{hertz}.png')
 	plt.show()
 
 
-for hertz in HERTZ:
-	for sides in SIDES:
+for sides in SIDES:
+	for hertz in HERTZ:
 		create_plot(sides, hertz)
